1aee89b27900e84c89b02ab46ca8c30e3e33bfae
RudyBecker <52896529+RudyBecker@users.noreply.github.com>
2020-03-24 11:58:00 -0700

    Write out ProgrammersIndex, Test passed 1 of 2

---
+++ programmers-preference-frontend/src/pages/ProgrammersIndex.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react'
+import { ListGroup, ListGroupItem, ListGroupItemHeading, ListGroupItemText } from 'reactstrap';
+
+const ProgrammersIndex = () => {
+    return(
+      <React.Fragment>
+        <ListGroup>
+          <ListGroupItemHeading>Programmer One</ListGroupItemHeading>
+          <ListGroupItemText>Programmer Age - Programmer Enjoys</ListGroupItemText>
+         </ListGroup>
+      </React.Fragment>
+    )
+}
+export default ProgrammersIndex
