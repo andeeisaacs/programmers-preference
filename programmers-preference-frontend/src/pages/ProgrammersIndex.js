4fec60587bd909771fbd28aab037f8c5b6b2469c
Ubuntu <ubuntu@ip-172-31-92-63.ec2.internal>
2020-03-24 22:06:49 +0000

    create dummy data and modify programmerindex

--- programmers-preference-frontend/src/pages/ProgrammersIndex.js
+++ programmers-preference-frontend/src/pages/ProgrammersIndex.js
@@ -1,14 +1,25 @@
 import React, { Component } from 'react'
 import { ListGroup, ListGroupItem, ListGroupItemHeading, ListGroupItemText } from 'reactstrap';
 
-const ProgrammersIndex = () => {
-    return(
-      <React.Fragment>
-        <ListGroup>
-          <ListGroupItemHeading>Programmer One</ListGroupItemHeading>
-          <ListGroupItemText>Programmer Age - Programmer Enjoys</ListGroupItemText>
-         </ListGroup>
-      </React.Fragment>
+class ProgrammersIndex extends Component {
+
+render(){
+  
+  const { id } = this.props.match.params
+  const programmer = this.props.programmers.find((programmer) => programmer.id === parseInt(id))
+
+return(
+{ this.props.programmers.map((programmer, index) => {
+  return(
+    <>
+    <ListGroup key={ index }>
+      <h4>{ programmer.name }</h4>
+      <small>{ programmer.age } - { programmer.enjoys }</small>
+    </ListGroup>
+    </>
     )
+  })}
+  )
+ }
 }
 export default ProgrammersIndex
---
+++ programmers-preference-frontend/src/programmers.js
@@ -0,0 +1,45 @@
+const programmers =[
+    {
+        id: 1,
+        name: 'Bill Gates',
+        age: 64,
+        enjoys: "hamburgers"
+    },
+    {
+        id: 2,
+        name: 'John Carmack',
+        age: 49,
+        enjoys: "Space stuff",
+    },
+    {
+        id: 3,
+        name: 'Bjarne Stroustrup',
+        age: 69,
+        enjoys: "Designing stuff sometimes"
+    },
+    {
+        id: 4,
+        name: 'Dennis Ritchie',
+        age: 0,
+        enjoys: "being dead",
+    },
+    {
+        id: 5,
+        name: 'Sid Meier',
+        age: 66,
+        enjoys: "Ruining your gaming experience."
+    },
+    {
+        id: 6,
+        name: 'Augusta Ada King',
+        age: 0,
+        enjoys: "also being dead"
+    },
+    {
+        id: 7,
+        name: 'John Romero',
+        age: 52,
+        enjoys: "Scary stuff ;)"
+    }
+    ]
+export default programmers
